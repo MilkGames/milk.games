75faba5a25242f7fc248351f321a5402a57a8ddf
MilkGames <memegames1337@gmail.com>
2020-12-07 00:23:13 +0100

    move to index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-#!/usr/bin/env node
-
 /**
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('milk.games:server');
 var http = require('http');
 
