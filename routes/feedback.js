f63fa759806b11e0cf2b7200969cc9c958e6e10f
MilkGames <memegames1337@gmail.com>
2020-12-26 04:44:21 +0100

    fix feedback

--- routes/feedback.js
+++ routes/feedback.js
@@ -11,8 +11,8 @@ module.exports = function(app, db) {
 
 	app.post('/feedback', rateLimit, function(req, res) {
 		if (req.body.feedback.length < 5) { return res.end("Feedback must be longer than 5 characters."); }
-		if (!config.debug) {
-			verify(config.cap, req.body["h-captcha-response"]).then(function(info){
+		if (!app.locals.config.debug) {
+			verify(app.locals.config.cap, req.body["h-captcha-response"]).then(function(info){
 				if (!info.success) { return res.end("Invalid Captcha"); } 
 				db.query('INSERT INTO feedback SET feedback = ?', [req.body.feedback], function (error, results, fields) { 
 					if (error) throw error;
